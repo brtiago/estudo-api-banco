fdc1d72f1a9258cf52d4b19dba925fbd97ecf367
Tiago <727320+brtiago@users.noreply.github.com>
2023-09-18 14:19:29 -0300

    feat: criação da entidade Transacao

---
+++ src/main/java/com/banco/api/domain/transacao/Transacao.java
@@ -0,0 +1,30 @@
+package com.banco.api.domain.transacao;
+
+import com.banco.api.domain.conta.Conta;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "transacoes")
+public class Transacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal valor;
+    @ManyToOne
+    private Conta origem;
+    @ManyToOne
+    private Conta destino;
+    private LocalDateTime hora;
+
+
+}
