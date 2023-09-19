207544c8dd4e17030a574cf3589b360b9f319e9f
tiago <tgribeiro@gmail.com>
2023-09-19 14:31:41 -0300

    refactor: incluindo getter e setters

--- src/main/java/com/banco/api/domain/conta/Conta.java
+++ src/main/java/com/banco/api/domain/conta/Conta.java
@@ -1,11 +1,19 @@
 package com.banco.api.domain.conta;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "contas")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
 public class Conta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
