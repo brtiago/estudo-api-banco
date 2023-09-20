e0920ebe2c5fbe3a7cdec6c05bea54b995f29391
tiago <tgribeiro@gmail.com>
2023-09-20 15:14:08 -0300

    feat: implementar o metodo excluir em ClienteController

--- src/main/java/com/banco/api/controller/ClienteController.java
+++ src/main/java/com/banco/api/controller/ClienteController.java
@@ -1,28 +1,45 @@
 package com.banco.api.controller;
 
 import com.banco.api.domain.cliente.Cliente;
+import com.banco.api.dto.AtualizacaoClienteDTO;
 import com.banco.api.dto.ClienteDTO;
 import com.banco.api.dto.ListagemClienteDTO;
 import com.banco.api.repository.ClienteRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/clientes")
 public class ClienteController {
     @Autowired
     private ClienteRepository repository;
 
     @Transactional
     @PostMapping("/cadastrar")
-    public void cadastrar(@RequestBody ClienteDTO dados) {
+    public void cadastrar(@RequestBody @Valid ClienteDTO dados) {
         repository.save(new Cliente(dados));
     }
     @GetMapping("/listar")
     public Page<ListagemClienteDTO> listar(Pageable paginacao){
         return repository.findAll(paginacao).map(ListagemClienteDTO::new);
     }
+
+    @Transactional
+    @PutMapping
+    public void atualizar(@RequestBody @Valid AtualizacaoClienteDTO dados){
+        var cliente = repository.getReferenceById(dados.id());
+        cliente.atualizarInformacoes(dados);
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id){
+        var cliente = repository.getReferenceById(id);
+        cliente.excluir();
+    }
+
 }
--- src/main/java/com/banco/api/domain/cliente/Cliente.java
+++ src/main/java/com/banco/api/domain/cliente/Cliente.java
@@ -1,5 +1,6 @@
 package com.banco.api.domain.cliente;
 
+import com.banco.api.dto.AtualizacaoClienteDTO;
 import com.banco.api.dto.ClienteDTO;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
@@ -26,12 +27,30 @@ public class Cliente {
     private String senha;
     @Enumerated(EnumType.STRING)
     private TipoCliente tipo;
+    private Boolean ativo;
 
     public Cliente(ClienteDTO dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.documento = dados.documento();
         this.email = dados.email();
         this.senha = dados.senha();
         this.tipo = dados.tipo();
+    }
+
+    public void atualizarInformacoes(AtualizacaoClienteDTO dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.email() != null) {
+            this.email = dados.email();
+        }
+        if (dados.senha() != null) {
+            this.senha = dados.senha();
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
