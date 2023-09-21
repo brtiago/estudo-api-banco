bfd5384465b1369199b99aa8606b98f614ae8891
tiago <tgribeiro@gmail.com>
2023-09-21 15:16:49 -0300

    feat: criação do metodo construtor

--- src/main/java/com/banco/api/dto/ContaDTO.java
+++ src/main/java/com/banco/api/dto/ContaDTO.java
@@ -1,5 +1,6 @@
 package com.banco.api.dto;
 
+import com.banco.api.domain.conta.Conta;
 import com.banco.api.domain.conta.TipoConta;
 import jakarta.validation.constraints.NotNull;
 
@@ -8,4 +9,8 @@ public record ContaDTO(
         Long idCliente,
         @NotNull
         TipoConta tipo
-) {}
+) {
+        public ContaDTO (Conta conta){
+                this(conta.getIdCliente(), conta.getTipo());
+        }
+}
