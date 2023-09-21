df804a87146ce02a82651aa8ae309e01430341df
tiago <tgribeiro@gmail.com>
2023-09-21 15:16:21 -0300

    feat: criação dos metodos listar e depositar

--- src/main/java/com/banco/api/controller/ContaController.java
+++ src/main/java/com/banco/api/controller/ContaController.java
@@ -1,12 +1,17 @@
 package com.banco.api.controller;
 
 import com.banco.api.domain.conta.Conta;
+import com.banco.api.dto.ClienteDTO;
 import com.banco.api.dto.ContaDTO;
+import com.banco.api.dto.DepositarDTO;
 import com.banco.api.repository.ContaRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/contas")
@@ -20,4 +25,20 @@ public class ContaController {
     public void cadastrar(@RequestBody ContaDTO contaDto){
         repository.save(new Conta(contaDto));
     }
+
+    @GetMapping("/listar")
+    public List<ContaDTO> listar() {
+        // return repository.findAll().stream().map(conta -> new ContaDTO(conta.getIdCliente(), conta.getTipo())).toList();
+        return repository.findAll().stream().map(ContaDTO::new).toList();
+    }
+
+    @Transactional
+    @PutMapping("/depositar")
+    public BigDecimal depositar(@RequestBody DepositarDTO dados) {
+        var conta = repository.getReferenceById(dados.id());
+        return conta.depositar(dados);
+    }
+
+
+
 }
