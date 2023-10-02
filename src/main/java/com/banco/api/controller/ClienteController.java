0f46c93f2d8f8fb4ae6b94d8750da290b5fa0d07
tiago <tgribeiro@gmail.com>
2023-10-02 19:01:37 -0300

    refactor: utilizando ResponseEntity para retornar códigos http

--- src/main/java/com/banco/api/controller/ClienteController.java
+++ src/main/java/com/banco/api/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.banco.api.controller;
 import com.banco.api.domain.cliente.Cliente;
 import com.banco.api.dto.AtualizacaoClienteDTO;
 import com.banco.api.dto.ClienteDTO;
+import com.banco.api.dto.DadosDetalhamentoClienteDTO;
 import com.banco.api.dto.ListagemClienteDTO;
 import com.banco.api.repository.ClienteRepository;
 import jakarta.transaction.Transactional;
@@ -10,7 +11,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/clientes")
@@ -20,26 +23,40 @@ public class ClienteController {
 
     @Transactional
     @PostMapping("/cadastrar")
-    public void cadastrar(@RequestBody @Valid ClienteDTO dados) {
-        repository.save(new Cliente(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid ClienteDTO dados, UriComponentsBuilder uriBuilder) {
+        var cliente = new Cliente(dados);
+        repository.save(cliente);
+
+        var uri = uriBuilder.path("/clientes/{id}").buildAndExpand(cliente.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoClienteDTO(cliente));
     }
     @GetMapping("/listar")
-    public Page<ListagemClienteDTO> listar(Pageable paginacao){
-        return repository.findAll(paginacao).map(ListagemClienteDTO::new);
+    public ResponseEntity<Page<ListagemClienteDTO>> listar(Pageable paginacao){
+        var page = repository.findAll(paginacao).map(ListagemClienteDTO::new);
+        return ResponseEntity.ok(page);
     }
 
     @Transactional
     @PutMapping("/{id}")
-    public void atualizar(@RequestBody @Valid AtualizacaoClienteDTO dados){
+    public ResponseEntity atualizar(@RequestBody @Valid AtualizacaoClienteDTO dados){
         var cliente = repository.getReferenceById(dados.id());
         cliente.atualizarInformacoes(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoClienteDTO(cliente));
     }
 
     @Transactional
     @DeleteMapping("/{id}")
-    public void excluir(@PathVariable Long id){
+    public ResponseEntity excluir(@PathVariable Long id){
         var cliente = repository.getReferenceById(id);
         cliente.excluir();
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var cliente = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoClienteDTO(cliente));
     }
 
 }
